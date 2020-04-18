d0413259c63c995a4b33bc0e21455b052646e0f8
axing <759837320@qq.com>
2020-04-18 17:49:25 +0800

    1、完成获取商品分页接口

--- routes/shop.js
+++ routes/shop.js
@@ -36,11 +36,32 @@ module.exports = [
         method:'GET',
         path:`/${GROUP_NAME}/{shopId}/goods`,
         handler:async (request,reply)=>{
-            reply();
+            // 增加带有 where 的条件查询
+            const { rows: results, count: totalCount } = await models.goods.findAndCountAll({
+                // 基于 shop_id 的条件查询
+                where: {
+                    shop_id: request.params.shopId,
+                },
+                attributes: [
+                    'id',
+                    'name',
+                ],
+                limit: request.query.limit,
+                offset: (request.query.page - 1) * request.query.limit,
+            });
+            reply({results,totalCount});
         },
         config:{
             tags:['api',GROUP_NAME],
             description:'获取店铺的商品列表',
+            validate:{
+                params: {
+                    shopId: Joi.string().required().description('店铺的id'),
+                },
+                query: {
+                    ...paginationDefine
+                }
+            }
         }
     }
 ]
